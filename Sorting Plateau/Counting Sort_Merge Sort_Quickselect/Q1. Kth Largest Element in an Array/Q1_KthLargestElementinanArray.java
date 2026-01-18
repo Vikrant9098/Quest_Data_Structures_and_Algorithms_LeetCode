d3c986300369d5b95d8ce32ed046e0c340279706
Vikrant Naik <vikrantnaik4802@gmail.com>
2026-01-18 10:53:34 +0530

    Q1_KthLargestElementinanArray.java

---
+++ Sorting Plateau/Counting Sort_Merge Sort_Quickselect/Q1. Kth Largest Element in an Array/Q1_KthLargestElementinanArray.java
@@ -0,0 +1,27 @@
+class Solution {
+    // Solution class
+
+    public int findKthLargest(int[] nums, int k) {
+        // Method to find the k-th largest element
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        // Min-heap to store k largest elements
+
+        for (int no : nums) {
+            // Loop through each number in the array
+
+            queue.add(no);
+            // Add current number to the heap
+
+            if (queue.size() > k) {
+                // If heap size exceeds k
+
+                queue.remove();
+                // Remove the smallest element
+            }
+        }
+
+        return queue.poll();
+        // Return the k-th largest element
+    }
+}
